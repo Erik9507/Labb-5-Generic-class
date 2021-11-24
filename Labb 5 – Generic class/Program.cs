044ab480100cd68764e5f173ad16b388ade688f2
Erik9507 <eriknorell1995@gmail.com>
2021-11-24 14:52:48 +0100

    Add project files.

---
+++ Labb 5 – Generic class/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Labb_5___Generic_class
+{
+    public class Check<T>
+    {
+        public bool check;
+        public bool toCheck(T x, T y)
+        {
+            return x.Equals(y);            
+        }
+    }  
+    public class Check1<T> : Check<T>
+    {        
+    }
+    public class Check2<T> : Check<T>
+    {
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Check1<string> ch1 = new Check1<string>();        
+            if (ch1.toCheck("test", "test"))//Equal
+            {
+                Console.WriteLine("equals");
+            }
+            else
+            {
+                Console.WriteLine("not equals");
+            }
+
+
+            Check1<string> ch2 = new Check1<string>();        
+            if (ch2.toCheck("test", "testing"))//not equal
+            {
+                Console.WriteLine("equals");
+            }
+            else
+            {
+                Console.WriteLine("not equals");
+            }
+
+
+            Check2<int> ch3 = new Check2<int>();         
+            if (ch3.toCheck(1, 1))//equal
+            {
+                Console.WriteLine("equals");
+            }
+            else
+            {
+                Console.WriteLine("not equals");
+            }
+
+
+            Check2<int> ch4 = new Check2<int>();            
+            if (ch4.toCheck(10, 20))//not equal
+            {
+                Console.WriteLine("equals");
+            }
+            else
+            {
+                Console.WriteLine("not equals");
+            }
+        }
+    }
+}
